65458c188c108e9b2788e138528eb1b4470e27a0
gichome <headgic@gmail.com>
2019-06-28 15:17:31 +0700

    All Basic Stuff (Header, Top, Bottom, and Footer)

--- src/App.js
+++ src/App.js
@@ -15,22 +15,35 @@ function App() {
   return (
     <div className="web-container">
       <header className="top-notif">
+        <div className="notif-container">
+          <div className="notif-details">
+            By accessing and using this website, you acknowledge that you have read and understand our <a href=".">Cookie Policy</a>, <a href=".">Privacy Policy</a>, and our <a href=".">Term of Service</a>.
+          </div>
+          <button className="notif-button">
+            Got it
+          </button>
+        </div>
       </header>
       <div className="top-content">
         <div className="logo-container">
           <img src="logo.png" alt="y-logo" className="app-logo"/>
         </div>
         <div className="top-details">
-          <h2>
-            Hello! I'm Yonathan Evan.
+          <h2
+            style={{fontWeight:"lighter", marginTop:15, marginBottom:15}}
+          >
+            Hello! I'm Yonathan Evan
           </h2>
-          <h2 className="bold">
+          <h2
+           className="bold"
+           style={{fontSize:"1.2em", marginTop:15, marginBottom:15}}
+          >
             Consult, Design, and Develop Websites
           </h2>
-          <p>
+          <p
+            style={{marginTop:15, marginBottom:15}}
+          >
             Have something great in mind? Feel free to contact me.
-          </p>
-          <p>
             I'll help you make it happen.
           </p>
           <button className="call-to-action">
@@ -38,23 +51,85 @@ function App() {
           </button>
         </div>
       </div>
-      <div className="botttom-content_grid">
-        <div className="bottom-header_row">
+
+      {/* Bottom section */}
+      <div className="botttom-content_grid"
+        style={{
+          backgroundColor:"rgb(230, 230, 230)",
+          color:"rgb(60, 60, 60)"
+        }}
+      >
+        <div className="bottom-header_column"
+          style={{
+            marginTop:80,
+            marginBottom:60,
+          }}
+        >
           <h2>
             How Can I Help You?
           </h2>
-          <p>
+          <p className="p-header">
             Our work then targeted, best practices outcomes social innovation synergy.
-          </p>
-          <p>
             Venture philanthropy, revolutionary inclusive policymaker relief. User-centered
-          </p>
-          <p>
             program areas scale.
           </p>
         </div>
-        <div className="bottom-body_row">
 
+        <div className="bottom-body_column"
+          style={{
+            marginBottom:80,
+          }}
+        >
+          <div className="bottom-body_container">
+            <div className="bb_content">
+              <h3 style={{margin:0, fontSize:"0.9em"}}>
+                Consult
+              </h3>
+              <p className="p-content">
+                Co-create, design thinking; strengthen infrastructure resist granular. Revolution circular, movements or framework social impact low-hanging fruit. Save the world compelling revolutionary progress.
+              </p>
+            </div>
+            <div className="bb_content">
+              <h3 style={{margin:0, fontSize:"0.9em"}}>
+                Design
+              </h3>
+              <p className="p-content">
+                Policymaker collaborates collective impact humanitarian shared value vocabulary inspire issue outcomes agile. Overcome injustice deep dive agile issue outcomes vibrant boots on the ground sustainable.
+              </p>
+            </div>
+            <div className="bb_content">
+              <h3 style={{margin:0, fontSize:"0.9em"}}>
+                Develop
+              </h3>
+              <p className="p-content">
+                Revolutionary ciruclar, movements a or impact framework social impact low-hanging. Save the compelling revolutionary inspire progress. Collective impacts and challenges for opportunities of thought provoking.
+              </p>
+            </div>
+            <div className="bb_content">
+              <h3 style={{margin:0, fontSize:"0.9em"}}>
+                Marketing
+              </h3>
+              <p className="p-content">
+                Peaceful; vibrant paradigm, collaborative cities. Shared vocabulary agile, replicable, effective altruism youth. Mobilize commitment to overcome injustice resilient, uplift social transparent effective.
+              </p>
+            </div>
+            <div className="bb_content">
+              <h3 style={{margin:0, fontSize:"0.9em"}}>
+                Manage
+              </h3>
+              <p className="p-content">
+                Change-makers innovation or shared unit of analysis. Overcome injustice outcomes strategize vibrant boots on the ground sustainable. Optimism, effective altruism invest optimism corporate social.
+              </p>
+            </div>
+            <div className="bb_content">
+              <h3 style={{margin:0, fontSize:"0.9em"}}>
+                Evolve
+              </h3>
+              <p className="p-content">
+                Activate catalzye and impact contextualize humanitarian. Unit of analysis overcome injustice storytelling altruism. Thought leadership mass incarceration. Outcomes bit data, fairness, social game-changer.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <footer className="footer">
