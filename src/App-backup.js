89041c6ccc836f5595b928b248dacceb9f96da7f
gichome <headgic@gmail.com>
2019-06-30 01:49:27 +0700

    Fixing newsletter-container

--- src/App-backup.js
+++ src/App-backup.js
@@ -9,7 +9,7 @@
 
 import React, {Component} from 'react';
 import './App.css';
-// import Newsletter from './Newsletter';
+import Newsletter from './Newsletter';
 
 const Topnotif = (props) => 
 <header>
@@ -26,55 +26,20 @@ const Topnotif = (props) =>
 
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      isHide:true,
-      showNotif: true,
-    };
-    this.isNewsletterHide = this.isNewsletterHide.bind(this)
-  }
-
-  /*
-    Show the isNewsletterHide after scrolling and
-    Only hide it after 10 minutes 
-  */
-
-  isNewsletterHide(){
-    let {isHide} = this.state;
-    window.scrollY > 500 && this.prev ?
-    isHide && this.setState({isHide:false})
-    :
-    !isHide && this.setState({isHide:false})
-    this.prev = window.scrollY;
-  }
-
-  componentDidMount(){
-    window.addEventListener('scroll',this.isNewsletterHide);
-  }
-
-  componentWillUnmount() {
-    window.addEventListener('scroll',this.isNewsletterHide);
-  }
 
   closeNotif = () => {
     this.setState({
       showNotif: false,
-    })
-    document.getElementsByClassName("top-notif")[0].style.padding = "0px";
-    ;
+    });
   };
-
-  closeNewsletter = () => {
-    this.setState({
-      isHide: true,
-    })
-    document.getElementsByClassName("newsletter-container")[0].style.display = "none";
-    ;
-  };
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      showNotif: true,
+    };
+  }
 
   render() {
-    let classwhenHide=this.state.isHide?"newsletterhide":""
     return (
       <div className="web-container">
         <div className="top-notif">
@@ -142,12 +107,6 @@ class App extends Component {
               <div className="bb_content">
                 <h3 style={{margin:0, fontSize:"0.9em"}}>
                   Consult
-                  <i 
-                    style={{fontSize:"1.5em"}}
-                    className="fa-content"
-                  >
-                    &#xf0e6;
-                  </i>
                 </h3>
                 <p className="p-content">
                   Co-create, design thinking; strengthen infrastructure resist granular. Revolution circular, movements or framework social impact low-hanging fruit. Save the world compelling revolutionary progress.
@@ -156,12 +115,6 @@ class App extends Component {
               <div className="bb_content">
                 <h3 style={{margin:0, fontSize:"0.9em"}}>
                   Design
-                  <i 
-                    style={{fontSize:"1.5em"}}
-                    className="fa-content"
-                  >
-                    &#xf1fc;
-                  </i>
                 </h3>
                 <p className="p-content">
                   Policymaker collaborates collective impact humanitarian shared value vocabulary inspire issue outcomes agile. Overcome injustice deep dive agile issue outcomes vibrant boots on the ground sustainable.
@@ -170,12 +123,6 @@ class App extends Component {
               <div className="bb_content">
                 <h3 style={{margin:0, fontSize:"0.9em"}}>
                   Develop
-                  <i 
-                    style={{fontSize:"1.5em"}}
-                    className="fa-content"
-                  >
-                    &#xf1b3;
-                  </i>
                 </h3>
                 <p className="p-content">
                   Revolutionary ciruclar, movements a or impact framework social impact low-hanging. Save the compelling revolutionary inspire progress. Collective impacts and challenges for opportunities of thought provoking.
@@ -184,12 +131,6 @@ class App extends Component {
               <div className="bb_content">
                 <h3 style={{margin:0, fontSize:"0.9em"}}>
                   Marketing
-                  <i 
-                    style={{fontSize:"1.5em"}}
-                    className="fa-content"
-                  >
-                    &#xf0a1;
-                  </i>
                 </h3>
                 <p className="p-content">
                   Peaceful; vibrant paradigm, collaborative cities. Shared vocabulary agile, replicable, effective altruism youth. Mobilize commitment to overcome injustice resilient, uplift social transparent effective.
@@ -198,12 +139,6 @@ class App extends Component {
               <div className="bb_content">
                 <h3 style={{margin:0, fontSize:"0.9em"}}>
                   Manage
-                  <i 
-                    style={{fontSize:"1.5em"}}
-                    className="fa-content"
-                  >
-                    &#xf0ae;
-                  </i>
                 </h3>
                 <p className="p-content">
                   Change-makers innovation or shared unit of analysis. Overcome injustice outcomes strategize vibrant boots on the ground sustainable. Optimism, effective altruism invest optimism corporate social.
@@ -212,12 +147,6 @@ class App extends Component {
               <div className="bb_content">
                 <h3 style={{margin:0, fontSize:"0.9em"}}>
                   Evolve
-                  <i 
-                    style={{fontSize:"1.5em"}}
-                    className="fa-content"
-                  >
-                    &#xf201;
-                  </i>
                 </h3>
                 <p className="p-content">
                   Activate catalzye and impact contextualize humanitarian. Unit of analysis overcome injustice storytelling altruism. Thought leadership mass incarceration. Outcomes bit data, fairness, social game-changer.
@@ -226,34 +155,7 @@ class App extends Component {
             </div>
           </div>
         </div>
-        <div className={classwhenHide+"newsletter-container"}>
-          <div className="newsletter-details">
-            <h3 style={{margin:0, fontSize:"0.9em"}}>
-              Get latest updates in web technologies
-            </h3>
-            <i 
-              style={{fontSize:"0.6em"}}
-              className="fa"
-              onClick={this.closeNewsletter}
-            >
-                &#xf00d;
-            </i>
-            <p className="p-newsletter">
-              I write articles related to web technologies, such as design trends, development tools, UI/UX case studies and reviews, and more. Sign up to my newsletter to get them all.
-            </p>
-          </div>
-          <div className="newsletter-form">
-            <input 
-              name="email" 
-              type="email" 
-              placeholder="Email address" 
-              className="newsletter-input"
-            />
-            <button className="newsletter-button">
-              Count me in!
-            </button>
-          </div>
-        </div>
+        <Newsletter/>
         <footer className="footer">
           © 2019 Yonathan Evan. All rights reserved.
         </footer>
