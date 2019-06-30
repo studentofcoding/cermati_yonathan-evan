25f4a1bd19a451fdf95869014ae7cce81bd63d89
gichome <headgic@gmail.com>
2019-07-01 00:13:55 +0700

    Published

---
+++ build/precache-manifest.f62de49dca1bc5cb844ee5818e8b58f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1e67f15b4af17d6404e97416375fa0",
+    "url": "/cermati_yonathan-evan/index.html"
+  },
+  {
+    "revision": "a58ffad4a11935f61398",
+    "url": "/cermati_yonathan-evan/static/css/main.85200b28.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/cermati_yonathan-evan/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "a58ffad4a11935f61398",
+    "url": "/cermati_yonathan-evan/static/js/main.f8d69db8.chunk.js"
+  },
+  {
+    "revision": "45a4ed2b808720320be6",
+    "url": "/cermati_yonathan-evan/static/js/runtime~main.edb26986.js"
+  },
+  {
+    "revision": "a5a74b4ff9be5b81b4ee10c0f456d8e9",
+    "url": "/cermati_yonathan-evan/static/media/bg-top.a5a74b4f.jpg"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cermati_yonathan-evan/precache-manifest.f62de49dca1bc5cb844ee5818e8b58f2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cermati_yonathan-evan/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
